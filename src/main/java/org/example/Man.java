f46fe21b77a720ca039b690da781a48dafb31ff5
Dr.宇中航 <963237684@qq.com>
2023-12-29 16:44:55 +0800

    IDEA 第二次提交

--- src/main/java/org/example/Man.java
+++ src/main/java/org/example/Man.java
@@ -4,4 +4,7 @@ public class Man {
     public void sayHello(){
         System.out.println("Hello everybody");
     }
+    public void eat(String food){
+        System.out.println("男人吃"+food);
+    }
 }
